2e661b2bb5df7ed77dec8ca2af2c472836525202
Eugene Choi <eugenechoi@Eugenes-MBP.localdomain>
2024-10-16 14:45:36 -0400

    Finished overall styling

--- src/Components/WeatherCard.tsx
+++ src/Components/WeatherCard.tsx
@@ -14,38 +14,136 @@ export default function WeatherCard() {
     };
 
 
-    return <div className="weather-card">
+    return <div className="weather-card" style={{backgroundColor:"rgb(120, 239, 255)"}}>
         <h3>Wednesday, Oct 16</h3>
+        
         <h2><span><FontAwesomeIcon icon={faLocationDot} /></span> Location?</h2>
         <h1>55 deg</h1>
-        <h2>Raining</h2>
+        <h3>Raining</h3>
 
-        <div className="container">
-            <div className="flex-row">
-                <p>Hour</p>
-                <p>Temp</p>
-                <p>Precipitation %</p>
+        <div className="hourly-card">
+            <div className="flex-row label">
+                <div className="flex-item"><p>Hour</p></div>
+                <div className="flex-item"><p>Temp</p></div>
+                <div className="flex-item"><p>Rain/Snow %</p></div>
             </div>
-            <div className="flex-row">
-                <p>10:00AM</p>
-                <p>52 deg</p>
-                <p>0%</p>
-            </div>
-            <div className="flex-row current">
-                <p>11:00AM</p>
-                <p>55 deg</p>
-                <p>0%</p>
-            </div>
-            <div className="flex-row">
-                <p>12:00PM</p>
-                <p>55 deg</p>
-                <p>0%</p>
-            </div>
-            <div className="flex-row">
-                <p>1:00PM</p>
-                <p>55 deg</p>
-                <p>0%</p>
+            <div className="scroll-container">
+                <div className="flex-row past">
+                    <div className="flex-item"><p>1:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>2:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>3:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>4:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>5:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>6:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>7:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>8:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>9:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row past">
+                    <div className="flex-item"><p>10:00AM</p></div>
+                    <div className="flex-item"><p>52 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row current">
+                    <div className="flex-item"><p>11:00AM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>12:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>1:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>2:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>3:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>4:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>5:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>6:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>7:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>8:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>9:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>10:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
+                <div className="flex-row">
+                    <div className="flex-item"><p>11:00PM</p></div>
+                    <div className="flex-item"><p>55 deg</p></div>
+                    <div className="flex-item"><p>0%</p></div>
+                </div>
             </div>
         </div>
-    </div>;
+    </div>
 }
--- style-guide.md
+++ style-guide.md
@@ -5,40 +5,49 @@ The designs were created to the following widths:
 - Mobile: 375px
 - Desktop: 1440px
 
-### Card dimensions
+### WeatherCard dimensions
 
-- Desktop: 
+Desktop: 
+- width: 480px (full width * 3/9)
+- height: 600px (full height * 6/9)
+
+### HourlyCard dimensions
+
+Desktop:
+- width: 360px
+- height: 360px
+
+
+### Padding | Margin
+8px | 32px
 
 ## Colors
 
 ### Main color is dependent on temperature
-- 40deg: Light blue
-- 50deg
-- 60deg
-- 70deg: Green
-- 80deg
-- 90deg
-- 100deg: Red
+- 40deg: Light blue rgb(120, 239, 255)
+- 55deg: Blue rgb(75, 75, 255)
+- 70deg: Green rgb(30, 154, 49)
+- 85deg: Yellow rgb(255, 225, 92)
+- 100deg: Red rgb(176, 26, 26)
 
-### Main color's hue is dependent on current hour
-- Darkest = 0:00
-- Brightest = 12:00
-- Back to darkest = 0:00
+Background gradient color:
+- Purple-gray: rgba(77, 77, 101, 0.7)
+
+### Background color's hue is dependent on current hour
+- Darkest = 0:00 = rgba(128, 128, 128);
+- Brightest = 12:00 = white
 
 ### Font colors
-- Primary: hsl(0, 0%, 100%)
-- Secondary: Slate 500: hsl(216, 15%, 48%) at 80% opacity
-- Tertiary: Slate 300: hsl(212, 45%, 89%) at 50% opacity?
+- white
+    - opacity: 1 | 0.5
+- gray: rgba(128, 128, 128, 0.5)
 
 ## Typography
 
 ### Font
 
 ### Sizes
-H1: 60px
-H2: 36px
-H3: 24px
-P: 14px | 20px
+14px | 20px | 24px | 36px | 60px
 
 ### Weights
 Default: 400
@@ -46,4 +55,4 @@ H2: 300
 
 ### Line-height
 Default: 1
-H1: 1.3
+1 | 1.3 | 1.5
