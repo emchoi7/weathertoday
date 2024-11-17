0c9f40fe43101988d55ee9960554da39c2b8a859
Eugene Choi <eugenechoi@Eugenes-MBP.localdomain>
2024-11-17 09:55:28 -0500

    Cleaned up Location component

--- src/Components/Location.tsx
+++ src/Components/Location.tsx
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-export default function Location(props: { children: any, err: string | null }) {
+export default function Location(props: { children: string, err: string | null }) {
+    const locationContent = <div><span><FontAwesomeIcon icon={faLocationDot} /></span>{" " +props.children}</div>;
     return <div className={props.err ? "error" : ""}>
         {props.err 
             ?<div>
-                <h3><span><FontAwesomeIcon icon={faLocationDot} /></span>{props.children}</h3>
+                <h3>{locationContent}</h3>
                 <p>{props.err}</p>
             </div>
-            : <h2><span><FontAwesomeIcon icon={faLocationDot} /></span>{props.children}</h2>
+            : <h2>{locationContent}</h2>
         }
     </div>;
 }
